6f8f92e173abed7f10eb854adffee9c1c70691b0
anirudhssidhu <anirudhssidhu@gmail.com>
2025-11-08 15:53:08 +0530

    mlflow with local host setup

---
+++ test.py
@@ -0,0 +1,10 @@
+import mlflow
+print("Printing tracking URI Scheme below")
+
+print(mlflow.get_tracking_uri())
+
+print('\n')
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+print("new tracking URI scheme below")
+print(mlflow.get_tracking_uri())
